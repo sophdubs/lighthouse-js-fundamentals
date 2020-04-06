0cbadc1917f9528b7d53685ae6283af4ca01ba15
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-04-06 01:29:12 -0400

    Added new functions

---
+++ loopy-lighthouse.js
@@ -0,0 +1,14 @@
+for (let i = 100; i < 201; i++) {
+  if (i % 3 === 0) {
+    if (i % 4 === 0) {
+      console.log('LoopyLighthouse');
+    } else {
+      console.log('Loopy');
+    }
+  } else if (i % 4 === 0) {
+    console.log('Lighthouse');
+  } else {
+    console.log(i);
+  }
+}
+
