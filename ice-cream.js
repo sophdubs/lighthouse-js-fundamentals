4cd773b0d84e6f25053b48052a55455f68f869bd
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-04-07 16:21:45 -0400

    finished Array ice-cream activity

---
+++ ice-cream.js
@@ -0,0 +1,11 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+const newFlavours = [...iceCreamFlavours, "root beer"];
+console.log(newFlavours);
+
+console.log(newFlavours[0]);
+
+console.log(newFlavours[newFlavours.length -1]);
+
+console.log(newFlavours.length);
