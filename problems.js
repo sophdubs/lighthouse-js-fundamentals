a8fb46adfb768de8033bf0d1708973ba96da8349
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-04-08 16:31:28 -0400

    Code evaluation worksheet for Looping-lastIndexOf exercise

--- problems.js
+++ problems.js
@@ -44,3 +44,18 @@
 //   return solution;
 // }
 
+// const lastIndexOf = function(arr, val) {
+//   let lastIndex = -1;
+//   for (let i = 0; i < arr.length; i++){
+//     if (arr[i] === val) {
+//       lastIndex = i;
+//     }
+//   }
+//   return lastIndex;
+// }
+// console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+// console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+// console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+// console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+// console.log(lastIndexOf([], 3), "=?", -1);
+
