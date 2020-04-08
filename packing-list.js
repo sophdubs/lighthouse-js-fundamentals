4e31db7d2088bf2319e62227845d52bdc6cf9c19
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-04-08 16:15:19 -0400

    Changed for loop to while loop

--- packing-list.js
+++ packing-list.js
@@ -1,8 +1,13 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
-  console.log(packingList[i]);
-}
+// for (let i = 0; i < packingList.length; i++) {
+//   console.log(packingList[i]);
+// }
 
-console.log(packingList[3]);
+// console.log(packingList[3]);
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+} 
