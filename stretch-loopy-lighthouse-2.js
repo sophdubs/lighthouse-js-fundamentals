9ddf8fcfa8141cd38d0a5c2e6317a80df84c1a29
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-04-08 16:38:38 -0400

    Completed stretch activity Loopy Lighthouse 2

---
+++ stretch-loopy-lighthouse-2.js
@@ -0,0 +1,19 @@
+//range = array of 2 numbers representing the start and end values for the loop
+//multiples = should be an array of 2 numbers representing the multiples you want to replace with words
+//words = should be an array of 2 strings representing the words that will replace the multiples
+const loopyLighthouse = function(range, multiples, words) {
+  for (let i = range[0]; i <= range[1]; i++) {
+    if (i % multiples[0] === 0) {
+      if (i % multiples[1] === 0) {
+        console.log(`${words[0]}${words[1]}`);
+      } else {
+        console.log(words[0]);
+      }
+    } else if (i % multiples[1] === 0) {
+      console.log(words[1]);
+    } else {
+      console.log(i);
+    }
+  }
+}
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
