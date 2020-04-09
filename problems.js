b9fb71e97c2f5759d60d453dfaa263ab69ba91b8
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-04-08 20:13:14 -0400

    Code evaluation problem worksheet for Objects- Driving major daisy exercise

--- problems.js
+++ problems.js
@@ -73,3 +73,32 @@
 //   return bins;
 // }
 // console.log(smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 }));
+
+const carPassing = function(cars, speed) {
+  const newCar = {
+    time: Date.now(),
+    speed
+  }
+  cars.push(newCar);
+  return cars;
+}
+
+const cars = [
+  {
+    time: 1568329654807,
+    speed: 40,
+  },
+  {
+    time: 1568329821632,
+    speed: 42,
+  },
+  {
+    time: 1568331115463,
+    speed: 35
+  }
+]
+
+const speed = 38
+
+carPassing(cars, speed);
+console.log(cars);
